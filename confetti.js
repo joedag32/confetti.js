b9ad6852d79b4ad1474a6ee15f0cb269211565d1
Joe D'Agostino <joedag32@gmail.com>
2020-10-02 12:56:10 -0400

    added ability to  set colors

--- confetti.js
+++ confetti.js
@@ -3,6 +3,7 @@ var confetti = {
 	speed: 2,			//set the particle animation speed
 	frameInterval: 15,	//the confetti animation frame interval in milliseconds
 	alpha: 1.0,			//the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
+	colors: ["30,144,255", "107,142,35", "255,215,0", "255,192,203", "106,90,205", "173,216,230", "238,130,238", "152,251,152", "70,130,180", "244,164,96", "210,105,30", "220,20,60"],	// array of colors of the confetti 
 	gradient: false,	//whether to use gradients for the confetti particles
 	start: null,		//call to start confetti animation (with optional timeout in milliseconds, and optional min and max random confetti count)
 	stop: null,			//call to stop adding confetti
@@ -26,7 +27,6 @@ var confetti = {
 	confetti.remove = removeConfetti;
 	confetti.isRunning = isConfettiRunning;
 	var supportsAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
-	var colors = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"];
 	var streamingConfetti = false;
 	var animationTimer = null;
 	var pause = false;
@@ -36,14 +36,17 @@ var confetti = {
 	var context = null;
 
 	function resetParticle(particle, width, height) {
-		particle.color = colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
-		particle.color2 = colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
+		particle.color = "rgba(" + confetti.colors[(Math.random() * confetti.colors.length) | 0] + ", " + (confetti.alpha + ")");
+		particle.color2 = "rgba(" + confetti.colors[(Math.random() * confetti.colors.length) | 0] + ", " + (confetti.alpha + ")");
 		particle.x = Math.random() * width;
 		particle.y = Math.random() * height - height;
 		particle.diameter = Math.random() * 10 + 5;
 		particle.tilt = Math.random() * 10 - 10;
 		particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
 		particle.tiltAngle = Math.random() * Math.PI;
+
+		console.log(particle.color);
+
 		return particle;
 	}
 
